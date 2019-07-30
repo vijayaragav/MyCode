bc5a53963d40445fc7a9d7499ae2ce3383b3b123
Vijaya Ragavan <vijay.harry11@gmail.com>
2019-07-30 23:48:41 +0530

    Apple or Orange

--- Demo.py
+++ Demo.py
@@ -1,3 +1,8 @@
-print('GitHub account is created')
-print('Test')
-print('Sync successful')
+from sklearn import tree
+features = [[150, 1], [170, 1], [130, 0], [140, 0]]
+label = [1, 1, 0, 0]
+cls = tree.DecisionTreeClassifier()
+cls = cls.fit(features, label)
+print(cls.predict([[150, 0]]))
+
+
