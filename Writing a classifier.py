47f68908b70c2769d1be3db72467165251b15cf8
Vijaya Ragavan <vijay.harry11@gmail.com>
2019-08-03 00:20:00 +0530

    Updated

--- Writing a classifier.py
+++ Writing a classifier.py
@@ -33,7 +33,6 @@ y = iris.target
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=.5)
 
-#from sklearn.neighbors import KNeighborsClassifier
 clf = ScrappyKNN()
 clf.fit(x_train, y_train)
 
