7d4bf4c596758a9618124bffa978f1dedc08e38a
Vijaya Ragavan <vijay.harry11@gmail.com>
2019-08-06 23:26:59 +0530

    Pandas basic

---
+++ pandas.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import style
+style.use('fivethirtyeight')
+df = pd.read_csv("C:\\Users\\HP\\Downloads\\Dataset\winequality-red.csv", index_col=0)
+ds = df.head(5)
+dsu = ds.reindex(columns=["volatile acidity", 'citric acid'])
+dsu.plot(kind='Bar')
+plt.show()
+
