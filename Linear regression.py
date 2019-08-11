eb8643d76bc8977527557a19cda463889e43191a
Vijaya Ragavan <vijay.harry11@gmail.com>
2019-08-11 12:01:04 +0530

    Linear updated

--- Linear regression.py
+++ Linear regression.py
@@ -1,5 +1,7 @@
 import pandas as pd
+import numpy as np
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
 df = pd.read_csv('C:\\Users\HP\\Downloads\\Dataset\\headbrain.csv')
 X = df['Head Size(cm^3)'].values
 Y = df['Brain Weight(grams)'].values
@@ -9,6 +11,14 @@ X = X.reshape((m, 1))
 reg = LinearRegression()
 reg = reg.fit(X, Y)
 Y_pred = reg.predict(X)
+
+import matplotlib.pyplot as plt
+plt.plot(X, Y_pred, color='red')
+plt.scatter(X, Y, color='Blue')
+plt.title('Headbrain')
+plt.xlabel('Head size')
+plt.ylabel
+plt.show()
 
 r2_score = reg.score(X, Y)
 print(r2_score)
