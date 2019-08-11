7e0b8b1b8884e46b00db1f0860c016248ae5b6d7
Vijaya Ragavan <vijay.harry11@gmail.com>
2019-08-11 12:43:11 +0530

    Poly updated

--- Polynomial.py
+++ Polynomial.py
@@ -3,11 +3,10 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import PolynomialFeatures
 ds = pd.read_csv('C:\\Users\\HP\\Downloads\\Dataset\\position_Salaries.csv')
-print(ds)
+#print(ds)
 
 x = ds.iloc[:,1:2].values
 y = ds.iloc[:,2].values
-#print(x)
 
 reg=PolynomialFeatures(degree=5)
 x_poly = reg.fit_transform(x)
@@ -16,7 +15,12 @@ from sklearn.linear_model import LinearRegression
 reg2=LinearRegression()
 reg2=reg2.fit(x_poly,y)
 y_predict = reg2.predict(x_poly)
-
+z = reg2.predict(reg.fit_transform([[7.5]]))
+print(z)
 plt.scatter(x,y,color='red')
+plt.scatter(7.5, z, color='green')
 plt.plot(x, y_predict, color='blue')
+plt.title('Polynomial Regression')
+plt.xlabel('Level')
+plt.ylabel('Salary')
 plt.show()
